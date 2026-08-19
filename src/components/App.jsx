95666effb1992575cb4a2caa4ae0bc3f5105ae17
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-19 21:32:22 +0200

    fix mobile styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import Form from "./Form";
 export default function App() {
   return (
     <div className="min-h-lvh flex bg-olive-50 dark:bg-zinc-700">
-      <div className="w-3xl m-auto rounded-2xl overflow-hidden text-black bg-olive-100 dark:text-olive-100 dark:bg-zinc-800">
+      <div className="w-3xl rounded-none min-h-full md:m-auto md:rounded-2xl overflow-hidden text-black bg-olive-100 dark:text-olive-100 dark:bg-zinc-800">
         <Header />
-        <main className="py-17.5 px-16 flex flex-col gap-12 ">
+        <main className="py-10 px-4 md:py-17.5 md:px-16 flex flex-col gap-12">
           <Form />
         </main>
       </div>
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ export default function Form() {
   return (
     <form className="flex gap-3 justify-center flex-col md:flex-row">
       <input
-        className="flex-2 rounded-lg pl-3 ring ring-zinc-300 bg-white focus:ring-zinc-600 dark:text-black dark:bg-olive-100 border-none outline-none "
+        className="flex-2 rounded-lg px-3 ring py-2 ring-zinc-300 bg-white focus:ring-zinc-600 dark:text-black dark:bg-olive-100 border-none outline-none "
         type="text"
         aria-label="add ingredient"
         placeholder="e.g. oregano"
