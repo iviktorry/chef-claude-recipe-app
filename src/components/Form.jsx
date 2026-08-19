07cf232b72d62167c1c498257d16b6efecb09f71
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-19 21:26:46 +0200

    add aria-label and media query for Form comp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,10 @@
 export default function Form() {
   return (
-    <form className="flex gap-3">
+    <form className="flex gap-3 justify-center flex-col md:flex-row">
       <input
         className="flex-2 rounded-lg pl-3 ring ring-zinc-300 bg-white focus:ring-zinc-600 dark:text-black dark:bg-olive-100 border-none outline-none "
         type="text"
+        aria-label="add ingredient"
         placeholder="e.g. oregano"
       />
       <button className="flex-1 bg-zinc-900 text-white px-9 py-2 rounded-lg dark:text-black dark:bg-olive-100">
