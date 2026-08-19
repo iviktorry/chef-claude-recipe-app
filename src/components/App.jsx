34d6f4596b8f926f78194d7b131fb0a674b31b50
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-19 20:51:19 +0200

    add Header component and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import Header from "./Header";
 
 export default function App() {
-  return <Header />;
+  return (
+    <div className="min-h-lvh flex bg-olive-50 dark:bg-zinc-700">
+      <div className="max-w-96 m-auto text-black bg-olive-100 dark:text-white dark:bg-zinc-800">
+        <Header />
+      </div>
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import chefClaudeIcon from "../assets/chef-claude-icon.svg";
 
-export default function Header(){
-    return (
-        <header>
-            <img src={chefClaudeIcon} alt="logo" />
-            <h1>Chef Claude</h1>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="flex gap-3 items-center w-full py-7 bg-white dark:bg-zinc-900">
+      <img src={chefClaudeIcon} alt="logo" />
+      <h1 className="text-3xl">Chef Claude</h1>
+    </header>
+  );
 }
